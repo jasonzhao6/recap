bf43f200cd93e811da5b5548e10f1b851fee5958
Jason Zhao <jasonzhao6@gmail.com>
2012-02-27 03:22:14 -0800

    fine tuning

--- app/assets/javascripts/browse.js
+++ app/assets/javascripts/browse.js
@@ -1,17 +1,20 @@
 // Ajax search
 function search() {
+  var $matches = $('#matches');
+  $matches.html('');
   var query = escape($('#search').val());
   $.get('/?ajax=true&q=' + query, function(data) {
-    $('#matches').html(data);
+    $matches.html(data);
   });
 }
 $('#content').delegate('#search', 'keyup', function() {
+  var $clearSearch = $('#clear-search');
+  $(this).val().length > 0 ? $clearSearch.show() : $clearSearch.hide()
   search();
-  $('#clear-search').show();
 });
 $('#content').delegate('#clear-search', 'click', function() {
-  $('#clear-search').hide();
   $('#search').val('');
+  $('#clear-search').hide();
   search();
 });
 $('#content').delegate('.form-search', 'submit', function(e) {
