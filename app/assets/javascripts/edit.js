c79c708d61ca98eef67bd9e033443afea72179c1
Jason Zhao <jasonzhao6@gmail.com>
2012-03-02 20:27:41 -0800

    fixed edit save behavior

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -5,10 +5,7 @@ $('#content').delegate('#clear-hash-tag', 'click', function() {
 
 // jQuery-ujs post # this is not necessary, but it makes server-side validation nice (error feedback without page refresh)
 $('#content').delegate('#edit_tweet', 'ajax:success', function(event, data, status, xhr) {
-  $.pjax({
-    url: $(this).attr('action'),
-    container: '#content'
-  })
+  window.location.replace($(this).attr('action'));
 });
 $('#content').delegate('#edit_tweet', 'ajax:error', function(event, data, status, xhr) {
   alert(data.responseText);
